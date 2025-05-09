88e23fb9a3684f69db9731ee8f0c4aa3e309b8b0
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2025-05-09 12:33:17 -0400

    add GET requests

--- ISS.js
+++ ISS.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
+});
+
+app.get("/api/data", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://api.wheretheiss.at/v1/satellites/25544"
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
 });
 
 app.listen(port, () => {
